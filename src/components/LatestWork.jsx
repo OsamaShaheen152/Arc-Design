c3af2d404e9d320b295bf2c60db931252c6c285e
osamashaheen <osamashaheen152@gmail.com>
2025-07-02 20:56:05 +0300

    v_7

--- src/components/LatestWork.jsx
+++ src/components/LatestWork.jsx
@@ -1,96 +1,96 @@
 import { Calendar, ExternalLink, Loader2, AlertCircle } from "lucide-react";
 import ProjectCard from "./ProjectCard";
 
+const PROJECTS = [
+  {
+    id: 1,
+    project_name: "Project 1",
+    images: [
+      "../assets/Project 1/bathroom_2.jpg",
+
+      "../assets/Project 1/bathroom_top_view.jpg",
+
+      "../assets/Project 1/bedroom_kids.jpg",
+
+      "../assets/Project 1/bedroom_master.jpg",
+
+      "../assets/Project 1/dining_room_1.jpg",
+
+      "../assets/Project 1/floor_plan.jpg",
+
+      "../assets/Project 1/kitchen.jpg",
+
+      "../assets/Project 1/living_room_1.jpg",
+
+      "../assets/Project 1/living_room_2.jpg",
+    ],
+  },
+  {
+    id: 2,
+    project_name: "Project 2",
+    images: [
+      "../assets/Project 2/bedroom_tv_area.jpg",
+
+      "../assets/Project 2/bedroom_twin_beds.jpg",
+
+      "../assets/Project 2/bedroom_view_1.jpg",
+
+      "../assets/Project 2/bedroom_view_2.jpg",
+
+      "../assets/Project 2/entryway_modern.jpg",
+
+      "../assets/Project 2/floor_plan_2.jpg",
+
+      "../assets/Project 2/floor_plan.jpg",
+
+      "../assets/Project 2/home_office.jpg",
+
+      "../assets/Project 2/living_room_modern.jpg",
+
+      "../assets/Project 2/living_room_overall_view.jpg",
+
+      "../assets/Project 2/living_room_tv_area.jpg",
+    ],
+  },
+  {
+    id: 3,
+    project_name: "Project 3",
+    images: [
+      "../assets/Project 3/bedroom_twin_beds.jpg",
+
+      "../assets/Project 3/bedroom_view_1.jpg",
+
+      "../assets/Project 3/dining_area_details.jpg",
+
+      "../assets/Project 3/floor_plan_2.jpg",
+
+      "../assets/Project 3/floor_plan.jpg",
+
+      "../assets/Project 3/living_room_dining_area_2.jpg",
+
+      "../assets/Project 3/living_room_dining_area.jpg",
+
+      "../assets/Project 3/living_room_view_1.jpg",
+
+      "../assets/Project 3/WhatsApp Image 2025-07-01 at 06.49.38_015f3d46.jpg",
+    ],
+  },
+  {
+    id: 4,
+    project_name: "Project 4",
+    images: [
+      "../assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.17_ccac9192.jpg",
+
+      "../assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.18_81c063fb.jpg",
+
+      "../assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.19_0309af2a.jpg",
+
+      "../assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.19_66640d53.jpg",
+    ],
+  },
+];
+
 const LatestWork = ({ openModalHandler }) => {
-  let PROJECTS = [
-    {
-      id: 1,
-      project_name: "Project 1",
-      images: [
-        "../src/assets/Project 1/bathroom_2.jpg",
-
-        "../src/assets/Project 1/bathroom_top_view.jpg",
-
-        "../src/assets/Project 1/bedroom_kids.jpg",
-
-        "../src/assets/Project 1/bedroom_master.jpg",
-
-        "../src/assets/Project 1/dining_room_1.jpg",
-
-        "../src/assets/Project 1/floor_plan.jpg",
-
-        "../src/assets/Project 1/kitchen.jpg",
-
-        "../src/assets/Project 1/living_room_1.jpg",
-
-        "../src/assets/Project 1/living_room_2.jpg",
-      ],
-    },
-    {
-      id: 2,
-      project_name: "Project 2",
-      images: [
-        "../src/assets/Project 2/bedroom_tv_area.jpg",
-
-        "../src/assets/Project 2/bedroom_twin_beds.jpg",
-
-        "../src/assets/Project 2/bedroom_view_1.jpg",
-
-        "../src/assets/Project 2/bedroom_view_2.jpg",
-
-        "../src/assets/Project 2/entryway_modern.jpg",
-
-        "../src/assets/Project 2/floor_plan_2.jpg",
-
-        "../src/assets/Project 2/floor_plan.jpg",
-
-        "../src/assets/Project 2/home_office.jpg",
-
-        "../src/assets/Project 2/living_room_modern.jpg",
-
-        "../src/assets/Project 2/living_room_overall_view.jpg",
-
-        "../src/assets/Project 2/living_room_tv_area.jpg",
-      ],
-    },
-    {
-      id: 3,
-      project_name: "Project 3",
-      images: [
-        "../src/assets/Project 3/bedroom_twin_beds.jpg",
-
-        "../src/assets/Project 3/bedroom_view_1.jpg",
-
-        "../src/assets/Project 3/dining_area_details.jpg",
-
-        "../src/assets/Project 3/floor_plan_2.jpg",
-
-        "../src/assets/Project 3/floor_plan.jpg",
-
-        "../src/assets/Project 3/living_room_dining_area_2.jpg",
-
-        "../src/assets/Project 3/living_room_dining_area.jpg",
-
-        "../src/assets/Project 3/living_room_view_1.jpg",
-
-        "../src/assets/Project 3/WhatsApp Image 2025-07-01 at 06.49.38_015f3d46.jpg",
-      ],
-    },
-    {
-      id: 4,
-      project_name: "Project 4",
-      images: [
-        "../src/assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.17_ccac9192.jpg",
-
-        "../src/assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.18_81c063fb.jpg",
-
-        "../src/assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.19_0309af2a.jpg",
-
-        "../src/assets/Project 4/WhatsApp Image 2025-07-01 at 07.20.19_66640d53.jpg",
-      ],
-    },
-  ];
-
   return (
     <div className="py-20">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -126,8 +126,6 @@ const LatestWork = ({ openModalHandler }) => {
           </div>
         )}
       </div>
-
-      {/* Project Modal */}
     </div>
   );
 };
